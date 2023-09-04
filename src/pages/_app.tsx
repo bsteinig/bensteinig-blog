0c0f95a8984ad39f0dff4870ffcee2c6a8936b05
Benjamin Steinig <bsteinig@umich.edu>
2023-09-03 23:14:49 -0700

    Styles work in dev and local build, but not production?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,17 +28,6 @@ export default function App(props: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;800&family=IBM+Plex+Serif:wght@300;400&family=Poppins:wght@200;300;400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <ColorSchemeProvider
         colorScheme={colorScheme}
